d155a9dea19418ac6a8736845225aa98323eaabc
alihassndev <alihassndev@gmail.com>
2025-05-05 13:55:34 +0500

    start backend

---
+++ NodeJs/Sheriyan School/script.js
@@ -0,0 +1,60 @@
+// Fundamentals of JS
+// Arrays and Objects
+// Function return
+// Async JS Coding
+
+// ==============================
+// Arrays
+// ==============================
+
+// let arr = [
+//   1,
+//   2,
+//   3,
+//   4,
+//   "Ali",
+//   { name: "Ali", age: 22 },
+//   true,
+//   function () {},
+//   [],
+// ];
+
+// foreach, map, filter, find, indexOf
+
+// arr.forEach((element) => {
+//   element += " " + "Hello";
+//   console.log(element);
+// });
+
+// let newArray = arr.map((val) => {
+//   return val + 10;
+// });
+
+// console.log(newArray);
+
+// let ans = arr.filter((val) => {
+//   if ((val & 1) != 1) return true;
+// });
+
+// console.log(ans);
+
+// let arr = [1, 2, 3, 2, 4];
+// let ans = arr.find((val) => {
+//   if (val % 2 == 0) {
+//     console.log("index of " + val + " is " + arr.indexOf(val));
+//     return val;
+//   }
+// });
+
+// console.log(ans);
+
+// ==============================
+// Objects
+// ==============================
+let obj = {
+  name: "ali",
+  age: 22,
+  address: "Sialkot",
+};
+
+console.log(obj);
