5872b7ddd09027ef1c3838c2c02854cb97013cd4
alihassndev <alihassndev@gmail.com>
2025-05-10 00:20:33 +0500

    notebook practice

---
+++ NodeJs/Sheriyan School/notes/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    console.log(files);
+    res.render("index", { files: files });
+  });
+});
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    `${req.body.description}`,
+    (err) => {
+      console.log("written");
+    }
+  );
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.get("/files/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, data) => {
+    res.render("show", {
+      filename: req.params.filename,
+      content: data,
+    });
+  });
+});
+
+app.listen("3000", () => {
+  console.log("Server is listening ...");
+});
