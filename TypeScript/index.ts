2f0bc562b8e5d05f9da12e487e0aa228afee32a5
alihassndev <alihassndev@gmail.com>
2025-06-29 22:40:31 +0500

    understanding basics with arrays

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -20,11 +20,7 @@ let score = 12;
 // if you wanna use variable like you do in js. Then this type is for you
 
 let num: any = 12;
-// console.log(num);
-
 num = "ali";
-// console.log(num);
-
 num = true;
 // console.log(num);
 
@@ -42,6 +38,7 @@ const add = (num1: number, num2: number) => {
 
 // console.log(add(2, 3));
 
+// parameter with default value
 const test = (greet: string = "guest") => {
   console.log(`hello, ${greet}`);
 };
@@ -58,10 +55,74 @@ const multiply = (num1: number, num2: number): number => {
 };
 
 const res = multiply(2, 3);
-console.log(res);
+// console.log(res);
 
 // ==============================
 
 // void in TS
+
+// ==============================
+
+const printMessage = (message: string): void => {
+  console.log(`Message: ${message}`);
+};
+
+// printMessage("What are you doing ?");
+
+// ==============================
+
+// Never keyword -> function (throws error, with infinite loop) or variable never have a value
+function throwError(msg: string): never {
+  throw new Error(msg);
+}
+
+function infiniteLoop(num: number): never {
+  while (true) {}
+}
+
+let x: never;
+
+// x = infiniteLoop(2);
+
+// ==============================
+
+// Arrays
+// 1. [] Notation
+// 2. Generic Notation
+
+const nums: number[] = [1, 2, 3, 4];
+const names: string[] = ["ali", "hassan", "true"];
+// const names: Array<string> = ["ali", "hassan"]; // old style
+
+// console.log(nums);
+// console.log(names);
+
+const items: string[] = [];
+items.push("laptop");
+items.push("iphone");
+
+// console.log(items);
+
+// ==============================
+
+// Multi-Dimensional Arrays
+
+const mul: number[][] = [[1, 2, 3, 4], [4, 2, 1, 5], nums];
+const tri: number[][][] = [
+  [
+    [1, 2, 3, 4],
+    [4, 2, 1, 5],
+  ],
+  [nums],
+];
+
+// for (let i = 0; i < mul.length; i++) {
+//   for (let j = 0; j < mul[i].length; j++) {
+//     console.log(`${mul[i][j]} /n`);
+//   }
+// }
+
+console.log(mul);
+console.log(tri);
 
 // ==============================
