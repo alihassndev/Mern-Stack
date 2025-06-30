31f2f3ff3f7bc0aa2a21aa0942f8f979487ccf58
alihassndev <alihassndev@gmail.com>
2025-06-30 23:59:50 +0500

    objects

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -122,7 +122,125 @@ const tri: number[][][] = [
 //   }
 // }
 
-console.log(mul);
-console.log(tri);
+// console.log(mul);
+// console.log(tri);
 
 // ==============================
+
+// Objects
+
+// ===========  Using Interfaces  ===============
+
+// interface Person {
+//   name: string;
+//   age: number;
+//   location: string;
+// }
+
+// interface User extends Person {
+//   status: string;
+//   isAdmin?: boolean;
+// }
+
+// ===========  Using type  ===============
+
+type Person = {
+  name: string;
+  age?: number;
+  location: string;
+};
+
+// intersection types in TS
+type User = Person & {
+  status?: string; // optional property
+};
+
+// ===========  End Declaration  ===============
+
+const person: Person = {
+  name: "Ali",
+  location: "Sialkot",
+};
+
+//  "??" -> this is nullish coalescing operator to give the default value to a null property
+// console.log(
+//   `Name: ${person.name}, age: ${person.age ?? 18} and location: ${
+//     person.location
+//   }`
+// );
+
+const user: User = {
+  status: "new user",
+  name: "Ali",
+  age: 20,
+  location: "Lahore",
+};
+
+// console.log(person);
+// console.log(user);
+
+// ================ factory function ==================
+// function myUser(): { name: string; age: number; location: string } {
+//   return {
+//     name: "ali",
+//     age: 21,
+//     location: "Gujranwala",
+//   };
+// }
+
+// const newUser = myUser();
+// console.log(newUser);
+
+// ========== Other and cleaner way to write factory function ==========
+interface Employee {
+  name: string;
+  age: number;
+  readonly location: string;
+}
+
+function myEmployee(): Employee {
+  return {
+    name: "Hassan",
+    age: 23,
+    location: "Sialkot",
+  };
+}
+
+const newEmp: Employee = myEmployee();
+
+// // gives an error because it is readonly property
+// newEmp.location = "Lahore"
+newEmp.name = "Ali hassan";
+
+// console.log(newEmp);
+
+// ==============================
+
+type Status = "active" | "inactive";
+type Age = number;
+
+type Animal = {
+  age: Age;
+  status: Status;
+};
+
+const newAnimal: Animal = {
+  age: 1,
+  status: "active",
+};
+
+// console.log(newAnimal);
+
+// ==============================
+
+// Union types -> this only work for type like we have in intersection types
+type myAge = number | string;
+
+let newAge: myAge = 12;
+newAge = "23";
+
+console.log(`Age: ${newAge}`);
+
+// ==============================
+
+const srt: (number | string)[] = [12, 3];
